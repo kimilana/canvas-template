f93aa62464eb5c3dbb52f8fabb033bd1022e73f9
ifreykim@gmail.com <ifreykim@gmail.com>
2022-08-03 21:49:08 -0400

    squares

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #canvas-template
+
+Javascript 2D Canvas template from the following tutorial:
+
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Drawing_graphics#active_learning_getting_started_with_a_canvas
+
+Webpage: kimilana.github.io/canvas-template
--- script.js
+++ script.js
@@ -11,4 +11,20 @@ const ctx = canvas.getContext('2d');
 //Color the canvas background.
 //Here we are setting a fill color using the canvas' fillStyle property (this takes color values just like CSS properties do), then drawing a rectangle that covers the entire canvas with the fillRect method(the first two parameters are the coordinates of the rectangle's top left-hand corner; the last two are the width and height you want the rectangle drawn at)
 ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0,0,width, height); 
+ctx.fillRect(0,0,width, height);
+
+//add a small red rectangle 50 pixels from the top and left of the canvas, 100 pixels wide and 150 pixels tall
+ctx.fillStyle = 'rgb(255,0,0)';
+ctx.fillRect(50, 50, 100, 150);
+
+//overlapping green rectangle
+ctx.fillStyle = 'rgb(0,255,0)';
+ctx.fillRect(75, 75, 100, 100);
+
+//transparent purple rectangle
+ctx.fillStyle = 'rgba(255,0,255,0.75)';
+ctx.fillRect(25, 100, 175, 50); 
+//Draw a stroke: a retangle that is just an outline
+ctx.strokeStyle = 'rgb(255, 255, 255)';
+ctx.lineWidth = 5; //default line width is 1
+ctx.strokeRect(25,25,175,200);
